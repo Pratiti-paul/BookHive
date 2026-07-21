9d8470003fb3b96f2d770ae7c23b6276781f74ce
Pratiti Paul <paulpratiti23@gmail.com>
2026-07-21 20:01:23 +0530

    basic structure

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-yellow-100">
+      <h1 className="text-5xl font-bold text-black">
+        BookHive 🐝
+      </h1>
+    </div>
+  )
+}
+
+export default App
