fd6f2cea1934ca85113695217c8701d957dba609
Pratiti Paul <paulpratiti23@gmail.com>
2026-07-24 14:25:21 +0530

    routing done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,7 @@
-
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-yellow-100">
-      <h1 className="text-5xl font-bold text-black">
-        BookHive 🐝
-      </h1>
-    </div>
-  )
+  return <AppRoutes />;
 }
 
-export default App
+export default App;
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-yellow-100">
+      <h1 className="text-5xl font-bold">
+        Welcome to BookHive 🐝
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
